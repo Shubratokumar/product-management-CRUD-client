8adc0d60c4d87e1a52138044363086e4b25dc4e4
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-23 19:07:32 +0600

    added handle delete button

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,13 +3,21 @@ import useProducts from '../../hooks/useProducts';
 
 const Products = () => {
     const [products] = useProducts([]);
+    const handleDelete = id =>{
+        console.log("Delete the Product", id)
+    }
     return (
         <div className='w-50 mx-auto'>
             <h2>Total product available now : {products.length}</h2>
             {
                 products.map(product => 
                     <ul product={product} key={product._id}>
-                        <li>Name : {product.name} ; Price : $ {product.Price} ; Quantity : {product.Quantity} .</li>
+                        <li>Name : {product.name} ; Price : $ {product.Price} ; Quantity : {product.Quantity} .
+                        <div>
+                            <button onClick={()=>handleDelete(product._id)} className='btn btn-danger'>X</button>{" "}
+                            <button className='btn btn-info'>Update</button>
+                        </div>
+                        </li>
                     </ul>
                     )
             }
